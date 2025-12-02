ade04b33a9b584a25fa1d8c3d17e205401c29cb4
fred wordie <fred@mildlyupset.com>
2025-12-02 17:15:29 +0100

    new seo

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -6,11 +6,6 @@ const pb = new PocketBase(URL);
 
 const authData = await pb.collection('users').authWithPassword(PB_USER, PB_PASS);
 export const load: PageServerLoad = async ({ params }) => {
-	console.log(
-		await pb.collection('stickers').getFullList({
-			sort: '-created'
-		})
-	);
 	return {
 		prompts: await pb.collection('prompts').getFullList({
 			sort: '-created',
